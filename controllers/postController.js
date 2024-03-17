091b0387fd7e0919245a66b6072da4718d135e4a
AnjasFedo <fedoaf98@gmail.com>
2024-03-17 17:26:07 +0700

    Controller with some Handlers

---
+++ controllers/postController.js
@@ -0,0 +1,98 @@
+const Post = require("../models/postModel");
+
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        posts,
+      },
+      result: posts.length,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
+const createPost = async (req, res) => {
+  try {
+    const post = await Post.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        post,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
+module.exports = {
+  getPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost,
+};
