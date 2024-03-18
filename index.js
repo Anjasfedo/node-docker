f3236d346d51217b4fd198b0a286588a20434ccf
AnjasFedo <fedoaf98@gmail.com>
2024-03-18 15:18:36 +0700

    Redis Images for Session Storage

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+let RedisStore = require("connect-redis").default;
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  post: REDIS_PORT,
+});
 const postRouter = require("./routes/postRoute");
 const userRouter = require("./routes/userRoute");
 
@@ -25,10 +35,24 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hi Developsssss</h1>");
+  res.send("<h1>Hi Develop</h1>");
 });
 
 app.use("/api/v1/posts", postRouter);
