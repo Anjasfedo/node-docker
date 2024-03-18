076903613dcfdc30c1b15767998e56347a1d2fd7
AnjasFedo <fedoaf98@gmail.com>
2024-03-18 14:58:49 +0700

    Login Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,9 @@ const bcrypt = require("bcryptjs");
 const signUp = async (req, res) => {
   const { username, password } = req.body;
 
-  const hashPassword = await bcrypt.hash(password, 12);
   try {
+    const hashPassword = await bcrypt.hash(password, 12);
+
     const user = await User.create({ username, password: hashPassword });
 
     res.status(201).json({
@@ -21,6 +22,32 @@ const signUp = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+
+    if (isCorrect) {
+      res.status(200).json({
+        status: "Succeed",
+      });
+    } else {
+      res.status(400).json({
+        status: "Failed",
+        message: "Incorrect Password",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+    });
+  }
+};
+
 module.exports = {
   signUp,
+  login,
 };
