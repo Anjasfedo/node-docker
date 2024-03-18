f80803945aebc939febf333e307d2378bacef0eb
AnjasFedo <fedoaf98@gmail.com>
2024-03-18 14:47:12 +0700

    Hash Password with Bcrypt.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,12 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 const signUp = async (req, res) => {
+  const { username, password } = req.body;
+
+  const hashPassword = await bcrypt.hash(password, 12);
   try {
-    const user = await User.create(req.body);
+    const user = await User.create({ username, password: hashPassword });
 
     res.status(201).json({
       status: "Succeed",
